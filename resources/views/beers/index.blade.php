adacf4455c7c92f173642eccaddde471837f0dc5
Alex Giorgio Asole <alexgiorgio.asole@gmail.com>
2021-03-18 17:09:53 +0100

    index table

--- resources/views/beers/index.blade.php
+++ resources/views/beers/index.blade.php
@@ -1,1 +1,38 @@
-Hello World!
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
+    <title>Beers</title>
+</head>
+<body>
+    <table class="table table-hover">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">Brand</th>
+            <th scope="col">Name</th>
+            <th scope="col">Gradation</th>
+            <th scope="col">Type</th>
+            <th scope="col">Brewery</th>
+            <th scope="col">Reference</th>
+          </tr>
+        </thead>
+        <tbody>
+            @foreach ($beers as $beer)
+            <tr>
+                <th scope="col">{{$beer->id}}</th>
+                <th scope="col">{{$beer->brand}}</th>
+                <th scope="col">{{$beer->name}}</th>
+                <th scope="col">{{$beer->gradation}}</th>
+                <th scope="col">{{$beer->type}}</th>
+                <th scope="col">{{$beer->brewery}}</th>
+                <th scope="col"><img src={{$beer->image}} alt=""></th>
+
+              </tr>
+            @endforeach
+        </tbody>
+      </table>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -14,4 +14,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/beers', 'BeerController');
+Route::resource('/beers', 'BeerController');
