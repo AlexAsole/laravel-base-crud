02976f10044ece5da190348dd90c4ab2caadae9e
Alex Giorgio Asole <alexgiorgio.asole@gmail.com>
2021-03-18 16:41:30 +0100

    web.php non funzionante

---
+++ database/migrations/2021_03_18_143323_create_beers_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBeersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('beers', function (Blueprint $table) {
+            $table->id();
+            $table->string('brand', 60);
+            $table->string('name', 60);
+            $table->float('gradation', 5,2);
+            $table->string('type', 60);
+            $table->string('brewery', 60);
+            $table->string('image', 2048);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('beers');
+    }
+}
